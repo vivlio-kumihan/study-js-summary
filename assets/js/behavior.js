d44e3d2d2158e49f9a1495794d84d8aef200e217
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-14 17:11:51 +0900

    スクロール・イベントを利用してヘッダーの表示を切り替える その1
    挙　動=> スクロールするとヘッダーが現れる。一番上まで戻るとヘッダーが再出現する。
    留意点=> 『position: fixed;』したらその要素はフロートする。
    	 『transform: translateY(100%)』としたら戻す時は『transform: none;』とする。
    注意点=> 以下の三つのメソッドは同じ。
    	let scroll = document.documentElement.scrollTop
    	let scroll = window.scrollY
    	let scroll = window.pageYOffset

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,15 +1,13 @@
-// 『window』オブジェクトに『scroll』をきっかけとして『.addEventListener』でインスタンスを生成させる。
+// スクロール・イベントについて　その1
 window.addEventListener("scroll", () => {
-  // このインスタンス内で『.scrollTop』メソッドを送って画面上辺からの距離を取得する。
-  let scroll = document.documentElement.scrollTop
+  let scroll = window.pageYOffset
 
-  // 任意の要素へ値を設定して、
   document.getElementById("scroll-position").textContent = scroll
 
-  // 任意の要素のあしらいを変更する。
-  if (scroll > 300) {
-    document.querySelector("ul").classList.add("bg-color-red")
+  if (scroll > 0) {
+    document.getElementById("header").classList.add("active")
   } else {
-    document.querySelector("ul").classList.remove("bg-color-red")
+    document.getElementById("header").classList.remove("active")
   }
+
 })
