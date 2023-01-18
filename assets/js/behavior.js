4a8c3b94ed50bfd25602697179fc1e21c82446e8
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-18 21:25:28 +0900

    スクロール・イベント その3　スクロールに応じて見出しと画像が左右からレイアウトされる

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,12 +1,20 @@
-// スクロール・イベントについて　その2
-let beforeScrollValue = 0
-const headerElm = document.querySelector("header")
+// スクロール・イベントについて　その3
+const targetElememt = document.querySelectorAll(".animationTarget");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > beforeScrollValue) {
-    headerElm.classList.add("inActive")
-  } else {
-    headerElm.classList.remove("inActive")
+// スクロールするする度に...
+document.addEventListener("scroll", function () {
+  // その時その時の画面上面から注目しているオブジェクト上辺までの距離を逐次計測する関数を得る。
+  for (let i = 0; i < targetElememt.length; i++) {
+    // getBoundingClientRect()とは、
+    // その時その時の画面上面から注目しているオブジェクト上辺までの距離を逐次計測する関数。
+    // プラス、オブジェクトが半分程度見える距離でフェードインするアニメーションが始まるように
+    // 『top』メソッド（？）で得る距離にオブジェクトの半分の高さを加えて調整する。
+    const getElementDistance = targetElememt[i].getBoundingClientRect().top + targetElememt[i].clientHeight * 0.6
+    console.log(getElementDistance)
+    // 開いている画面の高さ
+    //   = 注目しているオブジェクトが画面をスクロールして画面下から出てくる距離
+    if (window.innerHeight > getElementDistance) {
+      targetElememt[i].classList.add("show")
+    }
   }
-  beforeScrollValue = window.scrollY
 })
