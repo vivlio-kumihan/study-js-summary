95ad554b40a95041742ea437b45c6c821f97f61b
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-19 19:21:10 +0900

    スクロール・イベント その4 スクロールをきっかけにシャッター効果
    css => 普通ならa要素に幅高さなど指定するが、今回は親要素へつける。
    => セレクター『>』指定親要素の一階層下の子要素を修飾。
    => セレクター『+』指定要素の下に隣接する要素を修飾。
    => セレクター『~』指定要素の下にある同じ階層の要素すべてを修飾。
    => 『a』要素にブロック要素を持たせるために『display: flex;』を指定する。
    => 『background-image: url(path/to/imege)』での指定について、
    	イメージを包摂する要素の『領域』に対しての『高さ』を指定することで表示できる。
    	『width: 100%』では何も表示されない。
    	画像自体の高さという意味ではないことに留意する。
    => 『background-position: 10cm 10cm;』など、画像をトリミングしているイメージ。
    => ブロック性質を持った要素を左に寄せる『margin-left: auto』
    => 効果をかける要素を配列に格納しforEachで回す。
    js => これが肝　elem.getBoundingClientRect().top + elem.clientHeight * 0.5
    	キーワード）要素の画面の上辺からの距離、要素自体の高さの半分
    => これも肝　if (window.innerHeight > getElementDistance) {function}
    	キーワード）画面上辺から要素までの距離が画面の高さより小さくなったら処理を開始する。
    css=> 任意のクラスを付けた要素に前後に要素を付け足したい場合　::befor, ::after
    	任意の要素を全てレイヤーで覆いたい場合の基本の建て付け記述
    .animation-target::before {
      content: '';
      display: block;
    => position: absolute;を付けて親がreletiveだと思い込むのはダメ。必ず指定すること。
      position: absolute;
      top: 0;
      left: 0;
    
      width: 100%;
      height: 100%;
      z-index: 1;
    }
    js => keyframesでオブジェクトを移動させる（-100％=>100%）時、センターで動きを一時的に止めたい処理
    	@keyframes moveMask {
    	  0% {
    	    transform: translateX(-100%);
    	  }
    	  45%, 55% {
    	    transform: translateX(0%);
    	  }
    	  100% {
    	    transform: translateX(100%);
    	  }
    	}

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,20 +1,12 @@
-// スクロール・イベントについて　その3
-const targetElememt = document.querySelectorAll(".animationTarget");
+// スクロール・イベントについて　その4
+const monitorElements = document.querySelectorAll(".animation-target")
 
-// スクロールするする度に...
-document.addEventListener("scroll", function () {
-  // その時その時の画面上面から注目しているオブジェクト上辺までの距離を逐次計測する関数を得る。
-  for (let i = 0; i < targetElememt.length; i++) {
-    // getBoundingClientRect()とは、
-    // その時その時の画面上面から注目しているオブジェクト上辺までの距離を逐次計測する関数。
-    // プラス、オブジェクトが半分程度見える距離でフェードインするアニメーションが始まるように
-    // 『top』メソッド（？）で得る距離にオブジェクトの半分の高さを加えて調整する。
-    const getElementDistance = targetElememt[i].getBoundingClientRect().top + targetElememt[i].clientHeight * 0.6
-    console.log(getElementDistance)
-    // 開いている画面の高さ
-    //   = 注目しているオブジェクトが画面をスクロールして画面下から出てくる距離
+window.addEventListener("scroll", () => {
+  monitorElements.forEach((elem, idx) => {
+    const getElementDistance = elem.getBoundingClientRect().top + elem.clientHeight * 0.5
     if (window.innerHeight > getElementDistance) {
-      targetElememt[i].classList.add("show")
+      elem.classList.add("show")
     }
-  }
+
+  })
 })
