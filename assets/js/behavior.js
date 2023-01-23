86a9b979c23bbb0b46f71c0e9f38d99e68957981
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-24 08:44:30 +0900

    スクロール・イベント その5 スクロールをきっかけに要素を左右にフワッとレイアウトしながらサイトを。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,12 +1,19 @@
-// スクロール・イベントについて　その4
-const monitorElements = document.querySelectorAll(".animation-target")
+const targetElememt = document.querySelectorAll(".animationTarget");
 
-window.addEventListener("scroll", () => {
-  monitorElements.forEach((elem, idx) => {
-    const getElementDistance = elem.getBoundingClientRect().top + elem.clientHeight * 0.5
+// スクロールするする度に...
+document.addEventListener("scroll", function () {
+  // その時その時の画面上面から注目しているオブジェクト上辺までの距離を逐次計測する関数を得る。
+  for (let i = 0; i < targetElememt.length; i++) {
+    // getBoundingClientRect()とは、
+    // その時その時の画面上面から注目しているオブジェクト上辺までの距離を逐次計測する関数。
+    // プラス、オブジェクトが半分程度見える距離でフェードインするアニメーションが始まるように
+    // 『top』メソッド（？）で得る距離にオブジェクトの半分の高さを加えて調整する。
+    const getElementDistance = targetElememt[i].getBoundingClientRect().top + targetElememt[i].clientHeight * 0.6
+    console.log(getElementDistance)
+    // 開いている画面の高さ
+    //   = 注目しているオブジェクトが画面をスクロールして画面下から出てくる距離
     if (window.innerHeight > getElementDistance) {
-      elem.classList.add("show")
+      targetElememt[i].classList.add("show")
     }
-
-  })
+  }
 })
