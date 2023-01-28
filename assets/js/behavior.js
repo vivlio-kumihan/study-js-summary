292c349fcc8a57bd3b44ab83477fe6ea562d0523
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-28 12:58:39 +0900

    toggleでクラスの付け替え、クリック毎に現在時刻を表示させる。=> 時間の取得

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,15 +1,19 @@
 const thisButton = document.querySelector(".button"),
-      ul = document.querySelector(".preview")
-      greetArr = ["hello", "bonjour", "你好", "こんにちは"]
-      listElems = document.querySelectorAll(".preview li")
+      timeIndication = document.querySelector(".preview"),
+      dayOftheWeek = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
 
 thisButton.addEventListener("click", () => {
-  // クリックするボタンの背景色を変更して、
+  const thisTime = new Date(),
+        year     = thisTime.getFullYear(),
+        month    = thisTime.getMonth() + 1,
+        day      = thisTime.getDate(),
+        // 日曜日をインデックス0番として、各曜日のインデックスを返す。
+        week     = dayOftheWeek[thisTime.getDay()],
+        hours    = thisTime.getHours(),
+        minites  = thisTime.getMinutes(),
+        seconds  = thisTime.getSeconds()
+
   thisButton.classList.toggle("add-elem")
-  // ulのliへ配列の各値を代入してリストを生成し、
-  listElems.forEach((element, idx) => {
-    element.textContent = greetArr[idx]
-  })
-  // 表示させる。これを切り替えているのが => toggle
-  ul.classList.toggle("appear")
+  timeIndication.textContent = `現在は、${year}年${month}月${day}日${week}${hours}時${minites}分${seconds}秒`
+  timeIndication.classList.toggle("appear")
 })
