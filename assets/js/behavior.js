647480bd9b057e109528d5aa649b7003479963e3
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-30 05:58:16 +0900

    入力した値を型変換して足し算をしてみる。=> typeof, element.value, basic form layout

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,66 +1,31 @@
-// // 基礎知識　JSで属性を設定・取得・確認
+// ///////////////////
+// //　型
 
-// // 設定 => setAttribute("属性名", "値")
-// // 取得 => getAttribute("属性名") 状態を持っている値で返す。
-// // 確認 => hasAttribute("属性名") 状態を真偽値で返す。
+// // 型を明示する。　typeof
+// let input = document.getElementById("input").textContent
+// console.log(typeof input) 
+// // => string
+// // 文字列の結合になる。
+// document.getElementById("output").textContent = input + 1000
 
-// arr = document.querySelectorAll("ul li a")
-// arr.forEach(element => {
-//  //『target』属性があるものに『_brank』値を設定する。
-//  element.setAttribute("target", "_brank")
-
-//  // 『target』属性があるものを取得する。
-//  console.log(element.getAttribute("target"))
-
-//  // 『クラス』属性を持っているものを取得する。この時、クラスの『値』が返る。
-//  console.log(element.getAttribute("class"))
-
-//  // 『クラス』属性を持っているものを取得する。この時、クラスの『真偽値』が返る。
-//  console.log(element.hasAttribute("class"))
-// });
+// // 型を変換する。　Number(ins), String(ins)
+// console.log(Number(input))
+// input = Number(input)
+// // 数ととして計算する。
+// document.getElementById("output").textContent = input + 1000
 
 
-// 【例示】クラスの値で条件分岐し処理を振り分けるコード
-const anchors = document.querySelectorAll("li")
+// フォームから入力した値で足し算をしてみる。
+const inputSubmit = document.querySelector('input[type="submit"]'),
+      outPut = document.querySelector(".out-put p")
 
-function putsElem(ins, words) {
-  const ul = document.createElement("ul"),
-        li = document.createElement("li")
-  li.classList.add("second")
-  ins.appendChild(ul)
-  ul.appendChild(li)
-  return li.textContent = words
-}
+inputSubmit.addEventListener("click", (elem) => {
+  
+  elem.preventDefault()
 
-// // if版
-// anchors.forEach(element => {
-//   if (element.getAttribute("class") === "john") {
-//     putsElem(element, "yoko")
-//   } else if (element.getAttribute("class") === "paul") {
-//     putsElem(element, "rinda")
-//   } else if (element.getAttribute("class") === "george") {
-//     putsElem(element, "hello, george!")
-//   } else if (element.getAttribute("class") === "ringo") {
-//     putsElem(element, "hello, ringo!")
-//   }
-// })
+  const inputOne = document.getElementById("input1").value,
+        inputTwo = document.getElementById("input2").value
 
-// switch版
-anchors.forEach(element => {
-  switch (element.getAttribute("class")) {
-    case "john":
-      putsElem(element, "yoko")
-      break;
-    case "paul":
-      putsElem(element, "rinda")
-      break;
-    case "george":
-      putsElem(element, "hello, george!")
-      break;
-    case "ringo":
-      putsElem(element, "hello, ringo!")
-      break;
-    default:
-      break;
-  }
+  const sum = Number(inputOne) + Number(inputTwo)
+  outPut.textContent = sum
 })
