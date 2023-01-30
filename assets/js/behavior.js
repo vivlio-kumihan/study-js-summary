d6093a91ffd12b4902a227fe984de6e35e2017c8
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-01-31 07:28:01 +0900

    配列　数値の配列を合計する。=>Number(), Strign(), typeof, .value, .reduce(), .split()

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,31 +1,20 @@
-// ///////////////////
-// //　型
-
-// // 型を明示する。　typeof
-// let input = document.getElementById("input").textContent
-// console.log(typeof input) 
-// // => string
-// // 文字列の結合になる。
-// document.getElementById("output").textContent = input + 1000
-
-// // 型を変換する。　Number(ins), String(ins)
-// console.log(Number(input))
-// input = Number(input)
-// // 数ととして計算する。
-// document.getElementById("output").textContent = input + 1000
-
-
 // フォームから入力した値で足し算をしてみる。
-const inputSubmit = document.querySelector('input[type="submit"]'),
+const submit = document.querySelector('input[type="submit"]'),
       outPut = document.querySelector(".out-put p")
 
-inputSubmit.addEventListener("click", (elem) => {
-  
+// クリックイベントの中身は、変数を定義しているだけ。
+// それで足し算プログラムがうごくとは不思議。
+submit.addEventListener("click", (elem) => {
+
   elem.preventDefault()
 
-  const inputOne = document.getElementById("input1").value,
-        inputTwo = document.getElementById("input2").value
-
-  const sum = Number(inputOne) + Number(inputTwo)
-  outPut.textContent = sum
-})
+  const inputArray = document.getElementById("input1").value.split("+"),
+        // // reduce()を使った数値を持った配列の合計を出すやり方。
+        // total = inputArray.reduce(function (sum, element) {
+        //   return Number(sum) + Number(element)
+        // }, 0),
+        // ただ、変数定義でこの書き方は違和感があるので、無名関数を使って1行でやる。
+        total = inputArray.reduce((sum, element) => Number(sum) + Number(element), 0),
+        outPut = document.querySelector(".out-put p")
+        outPut.textContent = total
+  })
