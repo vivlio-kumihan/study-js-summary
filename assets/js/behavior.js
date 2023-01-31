d45ef92a433098d7c22e4525584c5b3626bb3b57
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-02-01 06:24:49 +0900

    配列　数値の配列を合計する。条件分岐を使って各段階の小計も出力する。=> reduce, option "0"有り
    => reduceの基本の書式
    	最後に『0』を入れているのが肝心なところ。だが、
    	これをすると1回目、2回目…と表記させたい場合に余計な条件分岐
    	を作らないといけなくなる。
    // reduceの書式
    // const result = arr.reduce((previous, current, index) => {
    //   function...
    //   ...
    //   ...
    //   return previous + current
    //    ↓計算を始める初期値を設定できる。
    // }, 0)

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,3 +1,12 @@
+// reduceの書式
+// const result = arr.reduce((previous, current, index) => {
+//   function...
+//   ...
+//   ...
+//   return previous + current
+//    ↓計算を始める初期値を設定できる。
+// }, 0)
+
 // フォームから入力した値で足し算をしてみる。
 const submit = document.querySelector('input[type="submit"]'),
       outPut = document.querySelector(".out-put p")
@@ -9,12 +18,17 @@ submit.addEventListener("click", (elem) => {
   elem.preventDefault()
 
   const inputArray = document.getElementById("input1").value.split("+"),
-        // // reduce()を使った数値を持った配列の合計を出すやり方。
-        // total = inputArray.reduce(function (sum, element) {
-        //   return Number(sum) + Number(element)
-        // }, 0),
-        // ただ、変数定義でこの書き方は違和感があるので、無名関数を使って1行でやる。
-        total = inputArray.reduce((sum, element) => Number(sum) + Number(element), 0),
-        outPut = document.querySelector(".out-put p")
-        outPut.textContent = total
-  })
+        ul = document.querySelector(".out-put ul")
+        
+  const total = inputArray.reduce((sum, element, idx) => {
+    const li = document.createElement("li")
+    let times = idx + 1
+    if (times < inputArray.length) {
+      li.textContent = `${times }回目の小計 => ${ Number(sum) + Number(element) }`
+    } else {
+      li.textContent = `合計 => ${ Number(sum) + Number(element) }`
+    }
+    ul.appendChild(li)
+    return Number(sum) + Number(element)
+  }, 0)
+})
