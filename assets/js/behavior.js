0ee0ad872842cd7d3ea851d9a5bde27533fa86bd
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-02-02 05:54:09 +0900

    配列　hashの中で条件にあった値を配列にして返す。表組で表記する。=> filter, some, every

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,34 +1,65 @@
-// reduceの書式
-// const result = arr.reduce((previous, current, index) => {
-//   function...
-//   ...
-//   ...
-//   return previous + current
-//    ↓計算を始める初期値を設定できる。
-// }, 0)
+const members = [
+  {
+    position: "社長",
+    name: "佐藤",
+    age: 45,
+  },
+  {
+    position: "部長",
+    name: "鈴木",
+    age: 38,
+  },
+  {
+    position: "課長",
+    name: "高橋",
+    age: 35,
+  },
+  {
+    position: "係長",
+    name: "田中",
+    age: 30,
+  },
+  {
+    position: "社員",
+    name: "渡辺",
+    age: 23,
+  },
+  {
+    position: "社員",
+    name: "伊東",
+    age: 22,
+  }
+]
 
-// フォームから入力した値で足し算をしてみる。
-const submit = document.querySelector('input[type="submit"]'),
-      outPut = document.querySelector(".out-put p")
+const memberUl = document.getElementById("member"),
+      resultUl = document.getElementById("result")
 
-// クリックイベントの中身は、変数を定義しているだけ。
-// それで足し算プログラムがうごくとは不思議。
-submit.addEventListener("click", (elem) => {
-
-  elem.preventDefault()
-
-  const inputArray = document.getElementById("input1").value.split("+"),
-        ul = document.querySelector(".out-put ul")
-        
-  const total = inputArray.reduce((sum, element, idx) => {
-    const li = document.createElement("li")
-    let times = idx + 1
-    if (times < inputArray.length) {
-      li.textContent = `${times }回目の小計 => ${ Number(sum) + Number(element) }`
-    } else {
-      li.textContent = `合計 => ${ Number(sum) + Number(element) }`
-    }
-    ul.appendChild(li)
-    return Number(sum) + Number(element)
-  }, 0)
+members.forEach(element => {
+  const li = document.createElement("li")
+  li.textContent = `${element.position}: ${element.name}, ${element.age}歳 `
+  memberUl.appendChild(li)
 })
+
+// filterで値を収集して配列で返す。
+const selectMember = members.filter(member => {
+  return member.age >= 30
+})
+console.log(selectMember)
+
+selectMember.forEach(hash => {
+  const li = document.createElement("li")
+  li.textContent = `${hash.position}: ${hash.name}, ${hash.age}歳 `
+  resultUl.appendChild(li)
+})
+
+// 『some』で、配列の中に一つでも値があれば『ture』を返す。
+const someData = members.some(member => {
+  return member.age > 20
+})
+console.log(someData)
+
+// 『every』で、配列の中で条件を全て満たした値があれば『ture』を返す。
+const everyData = members.every(member => {
+  return member.age > 20
+})
+console.log(everyData)
