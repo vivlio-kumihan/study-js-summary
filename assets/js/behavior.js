83dbc8e51b75f0629af666cfce01279f8f8bf7f0
Nobuyuki Takahiro <studio.quad9@gmail.com>
2023-02-06 08:47:08 +0900

    => splice

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,35 +1,47 @@
-// フォームから入力した値で足し算をしてみる。
-const submit = document.querySelector('input[type="submit"]'),
-      outPut = document.querySelector(".out-put"),
-      ul = document.querySelector(".out-put ul")
+// JSにおける配列の処理について。配列データの削除、挿入、差し替え => splice()
 
-// クリックイベントの中身は、変数を定義しているだけ。
-// それで足し算プログラムがうごくとは不思議。
-submit.addEventListener("click", (elem) => {
+const members = [
+  {
+    position: "社長",
+    name: "和田",
+    ruby: "わだ",
+    age: 45,
+  },
+  {
+    position: "部長",
+    name: "明石",
+    ruby: "あかし",
+    age: 38,
+  },
+  {
+    position: "課長",
+    name: "高木",
+    ruby: "たかぎ",
+    age: 35,
+  },
+  {
+    position: "係長",
+    name: "田中",
+    ruby: "たなか",
+    age: 30,
+  },
+  {
+    position: "社員",
+    name: "木戸",
+    ruby: "きど",
+    age: 23,
+  },
+  {
+    position: "社員",
+    name: "村田",
+    ruby: "むらた",
+    age: 22,
+  }
+]
 
-  elem.preventDefault()
-
-  const inputArray = document.getElementById("input1").value.split("+")
-
-  const total = inputArray.reduce((sum, element, idx) => {
-    const li = document.createElement("li")
-    if (idx < inputArray.length - 1) {
-      li.textContent = `${ idx }回目の小計 => ${ Number(sum) + Number(element) }`
-    } else {
-      li.textContent = `合計 => ${ Number(sum) + Number(element) }`
-    }
-    ul.appendChild(li)
-    outPut.classList.add("active")
-    return Number(sum) + Number(element)
-  })
+members.forEach(element => {
+  const li = document.createElement("li")
+  li.textContent = `${element.name}さん：${element.position}, ${element.age}歳`
+  console.log(li)
+  document.getElementById("members-list").appendChild(li)
 })
-
-
-// reduceの書式
-// const result = arr.reduce((previous, current, index) => {
-//   function...
-//   ...
-//   ...
-//   return previous + current
-//    ↓計算を始める初期値を設定できる。
-// }, 0)
