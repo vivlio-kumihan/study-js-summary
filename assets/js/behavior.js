91163cb4a966b4e6b3a709a9abe202485758be54
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-02-23 10:03:31 +0900

    objectを使いdl > dt + dd要素を追加する例題

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,47 +1,20 @@
-// JSにおける配列の処理について。配列データの削除、挿入、差し替え => splice()
 
-const members = [
-  {
-    position: "社長",
-    name: "和田",
-    ruby: "わだ",
-    age: 45,
-  },
-  {
-    position: "部長",
-    name: "明石",
-    ruby: "あかし",
-    age: 38,
-  },
-  {
-    position: "課長",
-    name: "高木",
-    ruby: "たかぎ",
-    age: 35,
-  },
-  {
-    position: "係長",
-    name: "田中",
-    ruby: "たなか",
-    age: 30,
-  },
-  {
-    position: "社員",
-    name: "木戸",
-    ruby: "きど",
-    age: 23,
-  },
-  {
-    position: "社員",
-    name: "村田",
-    ruby: "むらた",
-    age: 22,
-  }
-]
 
-members.forEach(element => {
-  const li = document.createElement("li")
-  li.textContent = `${element.name}さん：${element.position}, ${element.age}歳`
-  console.log(li)
-  document.getElementById("members-list").appendChild(li)
-})
+const fruits = {
+  apple: "A sweet fruit that grows on trees.",
+  orange: "A citrus fruit with a tough outer layer.",
+  banana: "A long, curved fruit with a soft inside."
+}
+
+const dl = document.getElementById("dl")
+for (const[key, value] of Object.entries(fruits)) {
+  const dt = document.createElement("dt")
+  dt.textContent = key
+  console.log(dt)
+  // dl.appendChild(dt)
+
+  const dd = document.createElement("dd")
+  dd.textContent = value
+  console.log(dd)
+  // dl.appendChild(dd)
+}
