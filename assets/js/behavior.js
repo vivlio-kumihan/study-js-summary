b0cb9d4714d38ae22833e57b35087c1add0cf744
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-21 21:00:23 +0900

    form要素 => select
    	=> dl, dt, dd　レイアウトを補助する要素の導入
    	=> instance.value value属性の値を参照する。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,20 +1,4 @@
-
-
-const fruits = {
-  apple: "A sweet fruit that grows on trees.",
-  orange: "A citrus fruit with a tough outer layer.",
-  banana: "A long, curved fruit with a soft inside."
-}
-
-const dl = document.getElementById("dl")
-for (const[key, value] of Object.entries(fruits)) {
-  const dt = document.createElement("dt")
-  dt.textContent = key
-  console.log(dt)
-  // dl.appendChild(dt)
-
-  const dd = document.createElement("dd")
-  dd.textContent = value
-  console.log(dd)
-  // dl.appendChild(dd)
-}
+let food = document.querySelector("#food");
+document.querySelector("#btn").addEventListener("click", function() {
+  console.log(food.value)
+}, false);
