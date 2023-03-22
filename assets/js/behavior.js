15e88b1fc12cf3f7aad0359e4726d92570284687
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-22 12:42:33 +0900

    form要素 => checkbox -single
    	instance.check => チェックされたかどうかを条件分岐で判定。

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,11 +1,8 @@
+let onOff = document.querySelector("#on-off")
 document.querySelector("#btn").addEventListener("click", function() {
-  let result = [];
-  let foods = document.querySelectorAll("input[name='food']");
-
-  for (let food of foods) {
-    if (food.checked) {
-      result.push(food.value);
-    }
+  if (onOff.checked) {
+    console.log(onOff.value)
+  } else {
+    console.log("チェックされていません。")
   }
-  console.log(result);
 }, false);
