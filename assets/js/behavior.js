7a6e1fe29758cb989177c46dbddc52a7e6800f90
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-03-22 12:32:45 +0900

    form要素 => checkbox -multiple

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,4 +1,11 @@
-let food = document.querySelector("#food");
 document.querySelector("#btn").addEventListener("click", function() {
-  console.log(food.value)
+  let result = [];
+  let foods = document.querySelectorAll("input[name='food']");
+
+  for (let food of foods) {
+    if (food.checked) {
+      result.push(food.value);
+    }
+  }
+  console.log(result);
 }, false);
