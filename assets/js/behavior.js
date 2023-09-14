7d9e5f2692bb721818690e66d5a8acabd2b67883
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-09-15 06:17:25 +0900

    scroll down, split paragraph

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,35 +1,19 @@
-// ////////////////////////////////////////////////
-// .lead-copy.per-char
+const para = document.querySelectorAll('.effect-para p');
 
-// 一文字ずつ現れる
-// ドキュメント上の該当文章を一文字ごとバラバラにしていく。
-// 属性『letter-spacing: none;』を最後の文字だけ字間のアキを取り去る
-function splitText(className) {
-  className.forEach(elem => {
-    let htmlContent = elem.innerHTML
-    let tmpText = ""
-    htmlContent.split('<br>').forEach((sentence, idx, arr) => {
-      let preText = Array.from(sentence).slice(0, -1)
-      let lastChar = Array.from(sentence).slice(-1)
-      preText.forEach((char) => {
-        tmpText += `<span>${char}</span>`
+const splitPara = () => {
+  Array.from(para).forEach((p) => {
+    const wrappedText = p.textContent.split('')
+      .map((char) => {
+        return char === ' ' ? `<span>${ '&nbsp;' }</span>` : `<span>${ char }</span>`;
       })
-      tmpText = `${ tmpText }<span class='remove-letter-spacing'>${ lastChar }</span>`
-      // 最後の部分以外はbrタグを追加
-      if (idx < arr.length - 1) {
-        tmpText += '<br>'
-      }
-    })
-    elem.innerHTML = tmpText
-  })
-}
+      .join('');
+    p.innerHTML = wrappedText;
+  });
+};
 
-// .lead-copy.per-charを収集する。
-const leadCopy = document.querySelectorAll('.lead-copy.per-char')
-// textContentをspanで1文字ずばバラバラにして要素へ格納する。
-splitText(leadCopy)
+splitPara(para);
 
-leadCopy.forEach(elem => {
+para.forEach(elem => {
   const tl = gsap.timeline({
     defaults: {
       opacity: 0,
@@ -38,10 +22,34 @@ leadCopy.forEach(elem => {
     },
     scrollTrigger: {
       trigger: elem,
-      start: '50% 80%',
-      // markers: true
+      start: 'top center',
+      markers: true
     }
   })
   tl.from(elem, {})
     .from(elem.children, {}, '-=0.25')
 })
+
+
+// // 一文字ずつ現れる
+// // ドキュメント上の該当文章を一文字ごとバラバラにしていく。
+// // 属性『letter-spacing: none;』を最後の文字だけ字間のアキを取り去る
+// function splitText(className) {
+//   className.forEach(elem => {
+//     let htmlContent = elem.innerHTML
+//     let tmpText = ""
+//     htmlContent.split('<br>').forEach((sentence, idx, arr) => {
+//       let preText = Array.from(sentence).slice(0, -1)
+//       let lastChar = Array.from(sentence).slice(-1)
+//       preText.forEach((char) => {
+//         tmpText += `<span>${char}</span>`
+//       })
+//       tmpText = `${ tmpText }<span class='remove-letter-spacing'>${ lastChar }</span>`
+//       // 最後の部分以外はbrタグを追加
+//       if (idx < arr.length - 1) {
+//         tmpText += '<br>'
+//       }
+//     })
+//     elem.innerHTML = tmpText
+//   })
+// }
