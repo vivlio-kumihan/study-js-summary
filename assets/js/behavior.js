fd7ed6e757df16fee282dcb2a160f2e65ae6231c
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-09-14 16:59:12 +0900

    slice string

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,40 +1,47 @@
-// メニューを開く関数
-const slideDown = (el) => {
-  el.style.height = 'auto' //いったんautoに
-  let h = el.offsetHeight //autoにした要素から高さを取得
-  el.style.height = h + 'px'
-  el.animate([ //高さ0から取得した高さまでのアニメーション
-    { height: 0 },
-    { height: h + 'px' }
-  ], {
-    duration: 300, //アニメーションの時間（ms）
-  })
-}
+// ////////////////////////////////////////////////
+// .lead-copy.per-char
 
-// メニューを閉じる関数
-const slideUp = (el) => {
-  el.style.height = 0
-}
-
-let activeIndex = null //開いているアコーディオンのindex
-
-//アコーディオンコンテナ全てで実行
-const accordions = document.querySelectorAll('ul.include-accordion')
-accordions.forEach((accordion) => {
-
-  //アコーディオンボタン全てで実行
-  const accordionBtns = accordion.querySelectorAll('button')
-  accordionBtns.forEach((accordionBtn, idx) => {
-    accordionBtn.addEventListener('click', (e) => {
-      activeIndex = idx //クリックされたボタンを把握
-      // parentNode => button < li 
-      e.target.parentNode.classList.toggle('active') //ボタンの親要素（=ul>li)にクラスを付与／削除
-      const content = accordionBtn.nextElementSibling //ボタンの次の要素（=ul>ul）
-      if(e.target.parentNode.classList.contains('active')){
-        slideDown(content) //クラス名がactive（＝閉じていた）なら上記で定義した開く関数を実行
-      }else{
-        slideUp(content) //クラス名にactiveがない（＝開いていた）なら上記で定義した閉じる関数を実行
+// 一文字ずつ現れる
+// ドキュメント上の該当文章を一文字ごとバラバラにしていく。
+// 属性『letter-spacing: none;』を最後の文字だけ字間のアキを取り去る
+function splitText(className) {
+  className.forEach(elem => {
+    let htmlContent = elem.innerHTML
+    let tmpText = ""
+    htmlContent.split('<br>').forEach((sentence, idx, arr) => {
+      let preText = Array.from(sentence).slice(0, -1)
+      let lastChar = Array.from(sentence).slice(-1)
+      preText.forEach((char) => {
+        tmpText += `<span>${char}</span>`
+      })
+      tmpText = `${ tmpText }<span class='remove-letter-spacing'>${ lastChar }</span>`
+      // 最後の部分以外はbrタグを追加
+      if (idx < arr.length - 1) {
+        tmpText += '<br>'
       }
     })
+    elem.innerHTML = tmpText
   })
+}
+
+// .lead-copy.per-charを収集する。
+const leadCopy = document.querySelectorAll('.lead-copy.per-char')
+// textContentをspanで1文字ずばバラバラにして要素へ格納する。
+splitText(leadCopy)
+
+leadCopy.forEach(elem => {
+  const tl = gsap.timeline({
+    defaults: {
+      opacity: 0,
+      ease: 'power1.inOut',
+      stagger: .1
+    },
+    scrollTrigger: {
+      trigger: elem,
+      start: '50% 80%',
+      // markers: true
+    }
+  })
+  tl.from(elem, {})
+    .from(elem.children, {}, '-=0.25')
 })
